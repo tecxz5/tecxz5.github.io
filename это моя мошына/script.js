0ca4c1f66e9195ab73d433defba2b26408408122
tecxz5 <zamulatimofei@yandex.ru>
2024-07-18 18:52:26 +0300

    джава скрипт

--- это моя мошына/script.js
+++ это моя мошына/script.js
@@ -1,22 +1,16 @@
-document.querySelector('a[href="#hello"]').addEventListener('click', function(event) {
-    event.preventDefault();
-    var helloElement = document.querySelector('.hello');
-    helloElement.scrollIntoView({ behavior: 'smooth' });
-  });
-
-document.querySelector('a[href="#about-me"]').addEventListener('click', function(event) {
+  document.querySelector('a[href="#about-me"]').addEventListener('click', function(event) {
     event.preventDefault();
     var aboutMeElement = document.querySelector('.about-me');
     aboutMeElement.scrollIntoView({ behavior: 'smooth' });
   });
 
-document.querySelector('a[href="#projects"]').addEventListener('click', function(event) {
+  document.querySelector('a[href="#projects"]').addEventListener('click', function(event) {
     event.preventDefault();
     var projectsElement = document.querySelector('.projects');
     projectsElement.scrollIntoView({ behavior: 'smooth' });
   });
 
-document.querySelector('a[href="#contact"]').addEventListener('click', function(event) {
+  document.querySelector('a[href="#contact"]').addEventListener('click', function(event) {
     event.preventDefault();
     var contactElement = document.querySelector('.contact');
     contactElement.scrollIntoView({ behavior: 'smooth' });
