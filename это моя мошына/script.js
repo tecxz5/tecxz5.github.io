b9f1debad26cc1545addabedb19a4064893d7ca1
tecxz5 <zamulatimofei@yandex.ru>
2024-07-20 14:00:13 +0300

    чутка прокачиваем прошую версию и нынешнюю

--- это моя мошына/script.js
+++ это моя мошына/script.js
@@ -16,6 +16,13 @@ document.querySelector('a[href="#contact"]').addEventListener('click', function(
     contactElement.scrollIntoView({ behavior: 'smooth' });
   });
 
+  document.querySelectorAll('.clickable').forEach(element => {
+    element.addEventListener('click', event => {
+        event.preventDefault();
+        window.open(element.querySelector('a').href, '_blank');
+    });
+  });
+
 document.addEventListener("DOMContentLoaded", function() {
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         document.body.classList.add('mobile');
