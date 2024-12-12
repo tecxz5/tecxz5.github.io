849ce4f774700ff52b703057c485b9a10a807220
tecxz5 <zamulatimofei@yandex.ru>
2024-12-12 23:12:45 +0300

    пока балуюсь

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'site')));
 
-// Стартовый маршрут
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'site', 'index.html'));
+// Мидлвар для динамической загрузки HTML страниц
+app.use((req, res, next) => {
+    const filePath = path.join(__dirname, 'site', `${req.path}.html`);
+
+    if (fs.existsSync(filePath)) {
+        res.sendFile(filePath); // Если файл существует, отправляем его
+    } else {
+        next(); // Если файл не найден, передаем обработку следующему мидлвару
+    }
 });
+
+// Стартовый маршрут (например, для главной страницы)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'site', 'index.html'));
+});
+
+// Обработка 404 - если не нашли страницу
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
 
 // Запуск сервера
 app.listen(port, () => {
