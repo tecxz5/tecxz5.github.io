30fcb2c3e8e68a11268273a3dd504ce10191664b
tecxz5 <zamulatimofei@yandex.ru>
2025-03-31 17:12:22 +0300

    в кое-то веки нормальный загрузочный экран

--- site/sources/loading/loading.js
+++ site/sources/loading/loading.js
@@ -1,25 +1,28 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     // Получаем параметры запроса из URL
     const urlParams = new URLSearchParams(window.location.search);
-
     const loadingElement = document.querySelector('loading');
-    document.body.classList.add('no-scroll');
 
-    // Проверяем, отсутствует ли параметр 'l'
-    if (!urlParams.has('l')) {
-        // Если параметр отсутствует, показываем элемент loading
-        loadingElement.style.display = 'flex'; // Показываем элемент
-        setTimeout(() => {
-            loadingElement.classList.add('hidden');
-            setTimeout(() => {
-                loadingElement.style.display = 'none';
-                document.body.classList.remove('no-scroll');
-            }, 1000);
-        }, 2000);
-    } else {
-        // Если параметр присутствует, скрываем элемент loading немедленно
-        loadingElement.classList.add('hidden');
-        loadingElement.style.display = 'none';
+    // Проверяем, есть ли параметр 'l'
+    if (urlParams.has('l')) {
         document.body.classList.remove('no-scroll');
+    } else {
+        document.body.classList.add('no-scroll');
+        const showLoadingScreen = (duration) => {
+            loadingElement.style.transition = 'opacity 0.5s ease-in-out';
+            loadingElement.style.display = 'flex';
+            loadingElement.classList.add('visible');
+
+            setTimeout(() => {
+                loadingElement.classList.add('hidden');
+                setTimeout(() => {
+                    loadingElement.classList.remove('visible');
+                    loadingElement.style.display = 'none';
+                    document.body.classList.remove('no-scroll');
+                }, 500);
+            }, duration);
+        };
+
+        showLoadingScreen(2000);
     }
 });
