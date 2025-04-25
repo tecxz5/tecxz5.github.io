491b056a4170d8aec73ae7a0f55a273715e4f456
tecxz5 <zamulatimofei@yandex.ru>
2025-04-25 23:36:38 +0300

    рефакторинг: удаление иконок и шрифтов, добавление нового скрипта для управления ресурсами

---
+++ site/sources/head.js
@@ -0,0 +1,70 @@
+const siteConfig = {
+    icons: {
+        defaultIcon: '/pictures/icon/t5.ico',
+        type: 'image/x-icon'
+    },
+    fonts: {
+        links: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com'
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com',
+                crossorigin: true
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Geologica:wght,CRSV,SHRP@500,1,100&display=swap'
+            }
+        ]
+    },
+};
+
+const siteResources = {
+    config: siteConfig,
+
+    /**
+     * Установка иконки для страницы
+     * @param {string} iconPath - путь к иконке (опционально)
+     */
+    setFavicon: function(iconPath = this.config.icons.defaultIcon) {
+        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+        link.type = this.config.icons.type;
+        link.rel = 'shortcut icon';
+        link.href = iconPath;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    },
+
+    /**
+     * Добавление ссылок на шрифты
+     */
+    setFonts: function() {
+        const head = document.getElementsByTagName('head')[0];
+        this.config.fonts.links.forEach(linkData => {
+            const link = document.createElement('link');
+            Object.entries(linkData).forEach(([key, value]) => {
+                link[key] = value;
+            });
+            head.appendChild(link);
+        });
+    },
+
+    /**
+     * Инициализация ресурсов при загрузке страницы
+     */
+    init: function() {
+        document.addEventListener('DOMContentLoaded', () => {
+            this.setFavicon();
+            this.setFonts();
+        });
+    }
+};
+
+// Автоматическая инициализация
+siteResources.init();
