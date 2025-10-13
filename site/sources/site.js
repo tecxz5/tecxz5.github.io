ef55eb8ff3e1fecf5d968a796c9ff1bedcc1efd8
tecxz5 <zamulatimofei@yandex.ru>
2025-10-13 20:48:42 +0300

    редиректы🤙 (везде слеши, тяжело)

--- site/sources/site.js
+++ site/sources/site.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
             <a href="/?l">tecxz5</a>
         </div>
         <div class="navigation">
-            <a href="pashalka" id="clicky">пасхалка</a>
+            <a href="/pashalka" id="clicky">пасхалка</a>
         </div>
     `;
 
     // HTML-код для footer
     const footerHTML = `
         <p id="copyright">©<img src="https://github.com/tecxz5/tecxz5/blob/main/copyright.gif?raw=true" onclick="showPopup('copyright')" loading="lazy">
-        <p><a href="design" id="footer-link">О сайте</a></p>
+        <p><a href="/design" id="footer-link">О сайте</a></p>
     `;
 
     // Находим элементы header и footer
